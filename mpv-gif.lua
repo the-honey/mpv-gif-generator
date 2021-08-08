933de28297918801e6e62fe73bc19c8b06f48f57
Dragoner7 <16624998+Dragoner7@users.noreply.github.com>
2021-08-09 00:56:47 +0200

    Added back missing local filename, success check

--- mpv-gif.lua
+++ mpv-gif.lua
@@ -135,8 +135,8 @@ function make_gif_internal(burn_subtitles)
     os.execute(args)
 
     -- then, make the gif
+    local filename = mp.get_property("filename/no-ext")
     local file_path = output_directory .. "/" .. filename
-    local file_path = output_directory .. filename
 
     -- increment filename
     for i=0,999 do
@@ -160,8 +160,13 @@ function make_gif_internal(burn_subtitles)
     args = string.format('ffmpeg -v warning -ss %s %s -t %s -i "%s" -i "%s" -lavfi "%s [x]; [x][1:v] paletteuse" -y "%s"', position, copyts, duration, esc(pathname), esc(palette), esc(trim_filters), esc(gifname))
     os.execute(args)
 
-    msg.info("GIF created.")
-    mp.osd_message("GIF created.")
+    local ok, err, code = os.rename(gifname, gifname)
+	if ok then
+	    msg.info("GIF created: " .. gifname)
+	    mp.osd_message("GIF created: " .. gifname)
+	else
+	    mp.osd_message("Error creating file, check CLI for more info.")
+	end
 end
 
 function set_gif_start()
